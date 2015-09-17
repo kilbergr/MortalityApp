59ba44a2c4721158137aa6be5cc47cd0f0522399
kilbergr <rekilberg@gmail.com>
2015-09-17 11:37:33 -0700

    moving forward slowly

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,4 +1,12 @@
 class OptionsController < ApplicationController
   def index
+  	@deaths = Death.all
+  	@demographics = Demographic.all 
+  	@figures = Figure.all
   end
+
+  def show
+  	@dem = Demographic.find_by_id(3000)
+  end
+
 end
