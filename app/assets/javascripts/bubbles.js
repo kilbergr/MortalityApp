564769e3e88e543c1f6f9edc5f8de028e3ae0739
kilbergr <rekilberg@gmail.com>
2015-09-27 11:48:03 -0700

    showing individual percentages

--- app/assets/javascripts/bubbles.js
+++ app/assets/javascripts/bubbles.js
@@ -13,27 +13,69 @@ var getData = function(){
 };
 
 $(document).on('ready page:load', function () {
-dataset = getData();
+  dataset = getData();
+  var width = 1400,
+  height = 500;
+  var svg = d3.select("svg").append("svg")
+            .attr("width", width)
+            .attr("height", height);
 
-  var svg = d3.select("svg");
-  debugger;
-  var circle = svg.selectAll(".circle")
-    .data(dataset[0]);
 
-
-  circle.exit().remove();
-
-  circle.enter().append("circle")
+  var circle = svg.selectAll("circle")
+    .data(dataset[0])
+    .enter().append("circle")
       .attr("r", (function(d) {return d*3}))
-      .style("fill", "#FFCFAD")
-      .style("stroke", "#B8AA95")
-      .style("opacity", .5);
+      .attr("cx", (function(d, i) {return i*50}))
+      .attr("cy", height/2)
+      .attr("class",  "bubble")
+      .on("mouseover", function(){return tooltip.style("visibility", "visible");})
+      .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+      .on("mouseout", function(){return tooltip.style("visibility", "hidden");
+    });
+    svg.selectAll("text")
+       .data(dataset[0])
+       .enter()
+       .append("text")
+       .text(function(d) {
+          return d;
+       })
+       .attr("x", function(d, i) {
+          return (i * 50)-2;
+       })
+       .attr("y", height/2)
+       .attr("font-family", "sans-serif")
+       .attr("font-size", "11px")
+       .attr("fill", "#B8AA95");
 
-  circle
-      .attr("cx", (function(d) {return d*20}))
-      .attr("cy", (function(d) {return 1/d*200}));
-      
-  circle.append("text")
-      .attr("class", "title")
-      .text("Top Causes of Death"); 
+  var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .data(dataset[0])
+    .text(function(d){
+        return d;
+      })
+    // .text("PERCENTAGE HERE");
+
+
+  // circle.exit().remove();
+
+ 
+  
+
 });
+
+//   var width = 800,
+  // height = 600;
+
+  // var canvas = d3.select("body").append("svg")
+  //             .attr("width", width)
+  //             .attr("height", height)
+  //             .append("g")
+  //               .attr("transform", "translate(50,50)");
+  // var pack = d3.layout.pack()
+  //             .size([width, height - 50])
+  //             .padding(10)
+
+
