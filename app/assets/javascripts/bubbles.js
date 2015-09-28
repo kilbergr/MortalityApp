f60e8816f58763bb0d5eb41db7313f74e53ab6c4
kilbergr <rekilberg@gmail.com>
2015-09-28 15:10:25 -0700

    altering bubbles

--- app/assets/javascripts/bubbles.js
+++ app/assets/javascripts/bubbles.js
@@ -299,20 +299,3 @@ d3.select("#dataset3").on("click",updateBubble3);
 d3.select("#dataset4").on("click",updateBubble4);
 })
 
-
-
-
-
-//   var width = 800,
-  // height = 600;
-
-  // var canvas = d3.select("body").append("svg")
-  //             .attr("width", width)
-  //             .attr("height", height)
-  //             .append("g")
-  //               .attr("transform", "translate(50,50)");
-  // var pack = d3.layout.pack()
-  //             .size([width, height - 50])
-  //             .padding(10)
-
-
